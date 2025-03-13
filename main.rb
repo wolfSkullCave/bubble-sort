d7d7508432b0b348d1f4040c23fdd4b5119cb88d
Kyle <67805222+wolfSkullCave@users.noreply.github.com>
2025-03-13 09:11:32 +0200

    Finished project

--- main.rb
+++ main.rb
@@ -1,1 +1,27 @@
-puts "hello world"
+# frozen_string_literal: true
+
+def test_file
+  puts 'hello world'
+end
+
+def bubble_sort(array)
+  array_length = array.size
+  return array if array_length <= 1
+
+  loop do
+    swapped = false
+
+    (array_length - 1).times do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
+      end
+    end
+
+    break unless swapped
+  end
+
+  array
+end
+
+puts bubble_sort([4, 3, 78, 2, 0, 2])
